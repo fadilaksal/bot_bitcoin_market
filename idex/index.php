9c97e518fae62e339a77adc7b155d0432e5f6498
Mocha. N. Fadillah <mochamadfadilah88@gmail.com>
2018-09-02 23:49:55 +0700

    add condition when private/group

--- idex/index.php
+++ idex/index.php
@@ -13,11 +13,19 @@
     //mengambil list exchange
     function getExchangeList()
     {
-        //untuk pengambilan list exchange tergantung pada api masing-masing, jadi tidak harus sama seperti ini
         $ticker = (array)json_decode(getData("https://api.idex.market/returnTicker"));
-        $ticker = array_slice($ticker, 0,25);
-        $exchangeList = array_keys($ticker);
-        $exchangeList = "List Tickers : \n /" . implode(" \n /", $exchangeList);
+        // $ticker = array_slice($ticker, 0,25);
+        // $exchangeList = array_keys($ticker);
+        // $exchangeList = "List Tickers : \n /" . implode(" \n /", $exchangeList);
+        $exchangeList = "List Tickers IDEX : ";
+        $i = 1;
+        foreach($ticker as $key => $value){
+            $exchangeList .= "\n $i. /$key ";
+            if($i == 100){
+                break;
+            }
+            $i++;
+        }
         // print_r($exchangeList);
         return $exchangeList;
     }
@@ -106,26 +114,32 @@
         if((isset($messageData->new_chat_participant) && $messageData->new_chat_participant->username == USERNAME) || (isset($messageData->left_chat_participant) && $messageData->left_chat_participant->username == USERNAME)){
             $data = array(
                 'chat_id' => $chatId,
-                'text' => "🎁🎁🎁Terimakasih telah menambahkan idexbot \nketik /list untuk melihat exchange yang ada",
+                'text' => "🎁🎁🎁Terimakasih telah menambahkan idexbot \nketik /help untuk melihat exchange yang ada",
             );
 
             $response = sendRequest('sendMessage', $data);
         }else{
             $messageText    = $messageData->text;
 
-            //memecah string, jika perintahnya ada @ nya, seperti bot yang ada digrup-grup
+            //memecah string, jika perintahnya dari grup
+            if($messageData->chat->type == 'group' || $messageData->chat->type == 'supergroup'){
             $perintah = explode('@', $messageText);
             //nah ini jika perintahnya ada @ nya, maka jumlah array perintah akan > 1
-            if(count($perintah) > 1){
-                //jika @ setelah perintah usernamenya sama, maka memanggil method set_perintah
-                if ($perintah[1] == USERNAME) {
-                    $hasil = setPerintah($perintah[0]);
+                if(count($perintah) > 1){
+                    //jika @ setelah perintah usernamenya sama, maka memanggil method set_perintah
+                    if ($perintah[1] == USERNAME) {
+                        $hasil = setPerintah($perintah[0]);
+                    } else {
+                        echo "test";
+                        $hasil = '';
+                    }
                 } else {
+                    echo "test2";
                     $hasil = '';
                 }
             } else {
-                //jika tidak ada @ nya, maka langsung set_perintah
-                $hasil = setPerintah($perintah[0]);
+                //jika bukan grup, maka langsung set_perintah
+                $hasil = setPerintah($messageText);
             }
 
             //jika hasilnya tidak kosong, maka akan diproses
@@ -148,24 +162,27 @@
     function setPerintah($perintah)
     {
         switch ($perintah) {
-            case '/list':
+            case '/listidex':
                 return getExchangeList();
                 break;
-            case '/start':
-                return "🎁🎁🎁 IDEXBOT 🎁🎁🎁\nketik /list untuk melihat exchange yang ada";
+            case '/help':
+                return "🎁🎁🎁 IDEXBOT 🎁🎁🎁\nketik /listidex untuk melihat exchange yang ada";
                 break;
             default:
                 $key = str_replace("/","", $perintah);
                 $ticker = (array)getExchangeData();
                 if(isset($ticker[$key])){
+                    $high = $ticker[$key]->high != null ? $ticker[$key]->high*100000000 : 0;
+                    $low = $ticker[$key]->low != null ? $ticker[$key]->low*100000000 : 0;
+                    $last = $ticker[$key]->last != null ? $ticker[$key]->last*100000000 : 0;
                     $returnMessage =  "IDEX EXCHANGE";
                     $returnMessage .= "\n ☕ Market : " . $key;
-                    $returnMessage .= "\n 🍏 High : " . $ticker[$perintah]->high ;
-                    $returnMessage .= "\n 🍒 Low : " . $ticker[$perintah]->low;
-                    $returnMessage .= "\n 🍍 Last Price : " . $ticker[$perintah]->last;
+                    $returnMessage .= "\n 🍏 High : " . $high;
+                    $returnMessage .= "\n 🍒 Low : " . $low;
+                    $returnMessage .= "\n 🍍 Last Price : " . $last;
                     return $returnMessage;
                 } else {
-                    return "Perintah tidak ditemukan, silahkan masukkan perintah yang lain 😁 $perintah";
+                    // return "$perintah Perintah tidak ditemukan, silahkan masukkan perintah yang lain 😁";
                 }
                 break;
         }
@@ -180,17 +197,12 @@
      //method untuk webhook
     $entityBody = file_get_contents('php://input');
     $request = json_decode($entityBody);
-    //print_r($request);
+    
     if(!$request){
         echo "request error";
     } elseif( !isset($request->update_id) || !isset($request->message) ) {
         echo "tidak ada update_id";
-    }
-    else
-    {
-        //print_r($request);
-        
+    } else{
         process_message($request->message);
     }
-     //print_r($pesanditerima);
 ?>
